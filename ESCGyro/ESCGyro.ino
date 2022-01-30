9c81729c411196881c07aa2ab9e384b1dd13a1dd
Firedorange717 <joshuapk1234@gmail.com>
2022-01-30 12:01:48 -0700

    Update ESCGyro.ino
    Incorporation of BNO055 IMU

--- ESCGyro/ESCGyro.ino
+++ ESCGyro/ESCGyro.ino
@@ -1,18 +1,9 @@
-#include <MadgwickAHRS.h>
 #include <PID_v1.h>
 #include <Servo.h>
-#include <Arduino_LSM9DS1.h>
 #include <Wire.h>
-
-//----Madgwick AHRS Setup/Variables
-Madgwick filter;
-unsigned long microsPerReading, microsPrevious;
-float aRX, aRY, aRZ; //Raw values
-float gRX, gRY, gRZ;
-float aX, aY, aZ; // Converted values
-float gX, gY, gZ;
-float roll, pitch, heading;
-unsigned long microsNow;
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 //----PID Variables
 double Pk = 2.0;
@@ -25,15 +16,22 @@ PID PID(&Input, &Output, &Setpoint, Pk, Ik , Dk, P_ON_M , DIRECT); // PID Setup
 
 //----Motor & Servo setup
 Servo motor;
-byte motorPin = 9; // signal pin for the ESC.
+byte motorPin = 10; // signal pin for the ESC.
 Servo servo;
-byte servoPin = 8; // servo signal pin
+byte servoPin = 9; // servo signal pin
 
 int servPos = 130; // 90 degrees is level for gyro platter 5-175 range
 int escSpeed = 900; // lowest signal aka off throttle 900 - 2000 pwm value
 
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   motor.attach(motorPin);
   servo.attach(servoPin);
@@ -45,13 +43,13 @@ void setup() {
   motor.writeMicroseconds(escSpeed); // send "stop" signal to ESC. Also necessary to arm the ESC.
   servo.write(servPos);
 
-  // Start the IMU and filter
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while (1);
   }
-
-  filter.begin(10);
 
   delay(1500); // Delay to allow the ESC to recognize the stopped signal.
 
@@ -60,80 +58,116 @@ void setup() {
 
   delay(3000); //Delay to allow gyro to get up to speed
 
-  // Initialize variables to pace updates to correct rate
-  microsPerReading = 1000000;
-  microsPrevious = micros();
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+  /* Optional: Display current status */
+  displaySensorStatus();
+
+  bno.setExtCrystalUse(true);
 }
 
 void loop() {
-  microsNow = micros();
-  if (microsNow - microsPrevious >= microsPerReading) {
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
 
-    readIMU();
+  /* Display the floating point data */
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.print("\tOut: ");
+  Serial.print(Output, 4);
+  /* New line for the next sample */
+  Serial.println("");
 
-    // convert from raw data to gravity and degrees/second units
-    aX = convertRawAcceleration(aRX);
-    aY = convertRawAcceleration(aRY);
-    aZ = convertRawAcceleration(aRZ);
-    gX = convertRawGyro(gRX);
-    gY = convertRawGyro(gRY);
-    gZ = convertRawGyro(gRZ);
+  Input = event.orientation.y;
+  PID.Compute();
+  Output = constrain(Output, -35, 35);
 
-    // update the filter, which computes orientation
-    filter.updateIMU(gX, gY, gZ, aX, aY, aZ);
+  servPos = 130 + Output; // add or subtract roll
+  motor.writeMicroseconds(escSpeed); // Send signal to ESC.
 
-    // print the heading, pitch and roll
-    roll = filter.getRoll();
-    pitch = filter.getPitch();
-    heading = filter.getYaw();
-
-    Serial.print("Orientation: ");
-    Serial.print(heading);
-    Serial.print(" ");
-    Serial.print(pitch);
-    Serial.print(" ");
-    Serial.println(roll);
-
-    Input = pitch;
-    PID.Compute();
-    Output = constrain(Output, -35, 35);
-
-    servPos = 130 + Output; // add or subtract roll
-    motor.writeMicroseconds(escSpeed); // Send signal to ESC.
-
-    // Increment previous time, so we keep proper pace
-    microsPrevious = microsPrevious + microsPerReading;
-  }
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
-
-/**
-  Read accel and gyro data.
-  returns true if value is 'new' and false if IMU is returning old cached data
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
 */
-bool readIMU() {
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() ) {
-    IMU.readAcceleration(aRX, aRY, aRZ);
-    IMU.readGyroscope(gRX, gRY, gRZ);
-    return true;
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
   }
-  return false;
-}
 
-float convertRawAcceleration(int aRaw) {
-  // since we are using 4 g range
-  // -4 g maps to a raw value of -32768
-  // +4 g maps to a raw value of 32767
-
-  float a = (aRaw * 4.0) / 32768.0;
-  return a;
-}
-
-float convertRawGyro(int gRaw) {
-  // since we are using 2000 degrees/seconds range
-  // -2000 maps to a raw value of -32768
-  // +2000 maps to a raw value of 32767
-
-  float g = (gRaw * 2000.0) / 32768.0;
-  return g;
+  /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.print(mag, DEC);
 }
